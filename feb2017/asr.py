9c76d7b011da90f38aee4c28effdfaa356ea3a45
Danijel Koržinek <danijel@pja.edu.pl>
2021-12-28 14:14:18 +0100

    Created description of feb2017 corpus

---
+++ feb2017/asr.py
@@ -0,0 +1,74 @@
+import torch
+import torchaudio
+from datasets import Dataset
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+from pyctcdecode import build_ctcdecoder
+import json
+import argparse
+from pathlib import Path
+
+if __name__=='__main__':
+
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--model',default='classla/wav2vec2-xls-r-parlaspeech-hr')
+    parser.add_argument('--device',default='cuda',help='"cuda or "cpu"')
+    parser.add_argument('--rate',type=float,default=16000)
+    parser.add_argument('vad_json',type=Path)
+    parser.add_argument('asr_json',type=Path)
+
+    args=parser.parse_args()
+
+    model_name = args.model
+    device = args.device
+    sampling_rate=args.rate
+
+    with open(args.vad_json) as f:
+        vad=json.load(f)
+
+    model = Wav2Vec2ForCTC.from_pretrained(model_name).to(device)
+    processor = Wav2Vec2Processor.from_pretrained(model_name)
+
+    vocab_dict = processor.tokenizer.get_vocab()
+    sorted_dict = {k.lower(): v for k, v in sorted(vocab_dict.items(), key=lambda item: item[1])}
+
+    decoder = build_ctcdecoder(list(sorted_dict.keys()),'lm.arpa',alpha=0.5,beta=1.0)
+
+    files={'sample':Path('sample.wav')}
+
+    ds_dict = {'file': [], 'start':[],'end':[]}
+    for file,ts in vad.items():
+        for seg in ts:
+            ds_dict['file'].append(file)
+            ds_dict['start'].append(seg['start'])
+            ds_dict['end'].append(seg['end'])
+            ds = Dataset.from_dict(ds_dict)
+
+    wavcache={}
+    def map_to_array(batch):
+        if batch['file'] in wavcache:
+            speech=wavcache[batch['file']]
+        else:
+            path = batch['file']
+            speech, _ = torchaudio.load(path)
+            speech = speech.squeeze(0).numpy()
+            wavcache[batch['file']]=speech
+        sstart=int(batch['start']*sampling_rate)
+        send=int(batch['end']*sampling_rate)
+        batch['speech'] = speech[sstart:send]
+        return batch
+
+    ds = ds.map(map_to_array)
+
+    def map_to_pred_lm(batch):
+        features = processor(batch["speech"], sampling_rate=sampling_rate, padding=True, return_tensors="pt")
+        input_values = features.input_values.to(device)
+        attention_mask = features.attention_mask.to(device)
+        with torch.no_grad():
+            logits = model(input_values,attention_mask=attention_mask).logits.cpu().numpy()[0]    
+        batch["predicted"] = decoder.decode(logits)
+        return batch
+
+
+    result_lm = ds.map(map_to_pred_lm,remove_columns=['speech'])
+
+    result_lm.to_json(args.asr_json)
---
+++ feb2017/vad.py
@@ -0,0 +1,92 @@
+import torch
+import json
+import argparse
+from pathlib import Path
+from math import ceil
+
+def resample(ts,max_gap=3,max_len=30):
+  new_ts=[]
+  for s in ts:
+    if s['end']-s['start']>max_len:
+      print(f'Error: max_len ({max_len}) is smaller than one of the segments ({s})!')
+      L=s['end']-s['start']
+      N=ceil(L/max_len)
+      P=L/N
+      for p in range(N-1):
+        ss=s['start']+p*P
+        new_ts.append({'start':ss,'end':ss+P})
+        print(f'Added {new_ts[-1]}')
+      new_ts.append({'start':s['start']+(N-1)*P,'end':s['end']})
+      print(f'Added {new_ts[-1]}')
+    else:
+      new_ts.append(s)
+  ts=new_ts
+  gts=[]
+  g=[ts[0]]
+  for s in ts[1:]:
+    if s['start']-g[-1]['end']<max_gap:
+      g.append(s)
+    else:
+      gts.append(g)
+      g=[s]
+  gts.append(g)
+  ret=[]
+  for g in gts:
+    l=g[-1]['end']-g[0]['start']
+    split_num=ceil(l/max_len)
+    if split_num>1:
+      min_len=l/split_num
+      start=g[0]['start']
+      end=g[0]['end']
+      for s in g[1:]:
+        if s['end']-start>max_len or end-start>min_len:
+          ret.append({'start':start,'end':end})
+          start=s['start']
+          end=s['end'] 
+        else:
+          end=s['end']
+      ret.append({'start':start,'end':end})
+    else:
+      ret.append({'start':g[0]['start'],'end':g[-1]['end']})
+  return ret
+
+
+if __name__=='__main__':
+
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--rate',type=float,default=16000)
+    parser.add_argument('--padding',type=int,default=1000)
+    parser.add_argument('vad_json',type=Path)
+    parser.add_argument('input_wav',nargs='+',type=Path)
+
+    args=parser.parse_args()
+
+    rate=args.rate
+    padding=args.padding
+
+    vad_model, vad_utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
+                                model='silero_vad',
+                                force_reload=True)
+
+    (get_speech_timestamps,
+    save_audio,
+    read_audio,
+    VADIterator,
+    collect_chunks) = vad_utils
+
+    output={}
+
+    for file in args.input_wav:
+        print(f'Processing {file}...')
+        wav = read_audio(str(file), sampling_rate=rate)
+        vad_ts=get_speech_timestamps(wav, vad_model, sampling_rate=rate, speech_pad_ms=padding, return_seconds=True)
+        ts=resample(vad_ts)
+        print(f'Found {len(ts)} segments.')
+        output[str(file)]=ts
+    
+    with open(args.vad_json,'w') as f:
+        json.dump(output,f)
+
+    
+
+
